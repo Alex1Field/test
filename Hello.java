70e04b41e4c6d69455f188b6f85ae7dd4e0e8d3a
Alexey <alexey.polev@yandex.ru>
2023-10-31 19:29:06 +0300

    Добавлен вывод 'A' в начале метода main()

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+	System.out.println("A");
         System.out.println("1");
         System.out.println("Hello, world!");
 	    System.out.println("2");
